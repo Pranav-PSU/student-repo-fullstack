f044b6426adcfe77133202d5e5906e6de9f9c1fd
dharamthokpranav <dharamthokpranav@gmail.com>
2022-10-04 23:23:04 -0700

    Code for fizzbuzz

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,10 +1,12 @@
 const fizzbuzz = (number) => {
+  let response = "";
   for (let i = 1; i <= number; i++) {
-    let response = "";
+    
     if (i % 3 === 0) response += "fizz";
     if (i % 5 === 0) response += "buzz";
     if (response === "") response = i;
     console.log(response);
+    response="";
   }
 };
 
