ea20e110b25e7022b81a39b9fa63206403bcb4e9
Pranav-PSU <pran4@pdx.edu>
2022-11-09 11:31:09 -0800

    Homework 4 compelted

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -9,10 +9,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
-  // Add your code here
+app.post('/submit', (req, res) => {
+    let text = '';
+    let value = '';
+    for (var key in req.body) {
+        value = req.body[key];
+        if (key === 'Newsletter') {
+            req.body[key] === 'true'
+                ? (value = 'Yes, I would like to join the newsletter')
+                : (value = 'No, thank you.');
+        }
+        text += `<h2>${key}: ${value}</h2>`;
+    }
+    res.set('Content-Type', 'text/html');
+    res.status(200).send(text);
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
